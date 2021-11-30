2f21d3e91e3fd2c57f4c24b950e56b22f4e717fe
Yura <mr.Shapovaloff3101@gmail.com>
2021-11-30 14:30:16 +0300

    fix

--- source/js/main.js
+++ source/js/main.js
@@ -29,6 +29,12 @@ window.addEventListener('DOMContentLoaded', () => {
   themeChange();
   initAccordion();
   initToggleMenu();
+  selectCustom.init();
+  initModals();
+  protfolioFilter();
+  scrollToElement();
+  initCardView();
+  smoothscroll.polyfill();
 
 
   // Modules
@@ -37,12 +43,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    selectCustom.init();
-    initModals();
-    protfolioFilter();
-    scrollToElement();
-    initCardView();
-    smoothscroll.polyfill();
+
   });
 });
 
